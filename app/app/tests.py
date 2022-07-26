4a2748b8180e636c941a1e7941d550037094f1d6
Charles Lejtman <charleslejtman@gmail.com>
2022-07-26 13:51:10 -0400

    Updated Docker Compose and CI/CD

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,16 +4,15 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
+
     def test_add_numbers(self):
         """test adding the numbers together"""
-        res=calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """test subtracting the numbers together"""
-        res=calc.subtract(10,3)
+        res = calc.subtract(10, 3)
 
-        self.assertEqual(res,7)
-
-
+        self.assertEqual(res, 7)
