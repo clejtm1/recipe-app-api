1b9aae160868403d6369e5b13a0b806727278314
Charles Lejtman <charleslejtman@gmail.com>
2022-07-26 13:07:23 -0400

    added the actual wait_for_db.py method

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
-        'USER': os.environ.get('DB_USER'),
         'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
