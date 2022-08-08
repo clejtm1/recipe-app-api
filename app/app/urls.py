23cb58a0b2f9c96fe3c3935ae56573dee839c8ab
Charles Lejtman <charleslejtman@gmail.com>
2022-08-08 12:16:43 -0400

    fix the url path

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView
-
+    SpectacularSwaggerView,
 )
+
 from django.contrib import admin
 from django.urls import path
 
@@ -25,8 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema')
     path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api_schema'
-        name='api-docs'),
+         'api/docs/',
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs',
     )
 ]
